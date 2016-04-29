ac99ff07c82e0905b2fbad5dd2393f68b011e0ea
rod7val <rod7val@gmail.com>
2016-04-29 00:59:14 -0300

    Directorio publico y favicon

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import favicon from 'serve-favicon'
 import path from 'path'
 
 const app = express()
 const port = process.env.PORT || 8080
 const IS_DEV = (process.env.NODE_ENV === 'development')
+
+app.use(favicon(__dirname + '/dist/favicon.ico'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/', (req, res) => res.send('Hello word'))
 
